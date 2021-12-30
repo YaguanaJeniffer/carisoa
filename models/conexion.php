7d5113c43c27a8c8f28a1553c0de54b036ab516d
Luxor-Fer <lzerna1972@uta.edu.ec>
2021-12-30 13:24:18 -0500

    proyecto

---
+++ models/conexion.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost"; 
+$username = "id18188624_usuariocari"; 
+$password = "Luis135713571357#"; 
+$dbname = "id18188624_carisoa"; 
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $dbname); 
+
+if(!$mysqli)
+{ 
+    die("Error en la conexion consulte al administrador".mysqli_connect_error());
+}
+else{
+    echo"nel prro";
+}
+
+?>
