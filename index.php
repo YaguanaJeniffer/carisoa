259b07a0c0492518a5eb754ea65da20b693fdf76
mirandaLopezAbraham1 <86499239+mirandaLopezAbraham1@users.noreply.github.com>
2021-12-30 22:04:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 require_once "models/model.php";
 $mvc = new MvcController();
 /$mvc-> plantilla();*/
-
+//
 require_once "controllers/controllerPlanta.php";
 require_once "modelsPlanta/modelPlanta.php";
 $mvc = new MvcController();
