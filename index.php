c2ecbe658c9c8d0573b32754fe023c3ceaa00a03
jess026p <67206641+jess026p@users.noreply.github.com>
2021-12-30 22:51:47 -0500

    prueba1
    verificar commit

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require_once "models/model.php";
 $mvc = new MvcController();
 /$mvc-> plantilla();*/
-//
+//sucursal
 require_once "controllers/controllerPlanta.php";
+//planta
 require_once "models/model.php";
 $mvc = new MvcController();
 $mvc-> plantilla();
