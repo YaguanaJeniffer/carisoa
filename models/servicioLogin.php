9059ab211c1461bddc87c48ad679d4ff9bdf2eb6
mirandaLopezAbraham1 <abrancito09@hotmail.com>
2022-01-01 13:52:56 -0500

    Cambio del servicio

--- models/servicioLogin.php
+++ models/servicioLogin.php
@@ -1,23 +1,37 @@
 <?php
+header('Acces-Control-Allow-Origin:*');
+header('Acces-Control-Allow-Headers: Origin, X-Requested-with, Content-Type, Accept, Authorization');
+header('Content-Type: application/json');
+header('Acces-Control-Allow-Methods: POST, GET, OPTIONS ');
+
 include 'conexion.php';
-$conn=new Conexion();
-if ($_SERVER['REQUEST_METHOD']=='POST'){
+
+if ($_SERVER['REQUEST_METHOD']==='POST'){
     if (isset($_POST['user']) && isset($_POST['psw']) && isset($_POST['tipo']) ){
         $sql="";
         $user=$_POST['user'];
         $psw=$_POST['psw'];
         $tipo=$_POST['tipo'];
         if ($tipo == 1){
-            $sql="SELECT * FROM sucursal WHERE NOM_SUC= $user AND COD_SUC = $psw";
+            $sqlSelect="SELECT * FROM sucursal WHERE NOM_SUC=$user AND COD_SUC =$psw";
         }else{
-            $sql="SELECT * FROM planta WHERE NOM_PLA= $user  AND COD_PLA = $psw";
+            $sqlSelect="SELECT * FROM planta WHERE NOM_PLA=$user  AND COD_PLA =$psw";
         }
-        $sqlcons=$conn->prepare($sql);
-        $sqlcons->execute();
-        $sqlcons->setFetchMode(PDO::FETCH_ASSOC);
-        //header('HTTP/1.1 200');
-        echo json_encode($sqlcons->fetchAll());
-        exit;
+        $respuesta= $conn->query($sqlSelect);
+        $result = array();
+        if($respuesta->num_rows > 0)
+        {
+            while($usuarios = $respuesta->fetch_assoc())
+            {
+                array_push($result, $usuarios);
+            }
+        }
+        else
+        {
+            $result = "No hay Usuario";
+        }
+        echo json_encode($result);
     }
+
 
 }
