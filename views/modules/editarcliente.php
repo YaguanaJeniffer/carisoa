51c183d6b157b86b4204ca54f041ef25e1931906
Jeniffer Yaguana <jyaguana3697@uta.edu.ec>
2022-01-07 15:11:11 -0500

    cambios sin importancia
    cambios easyui

--- views/modules/editarcliente.php
+++ views/modules/editarcliente.php
@@ -1,1 +1,14 @@
-editar clientes prro
+
+
+
+<div style="margin:20px 0;"></div>
+<div class="easyui-panel" style="width:100%;max-width:400px;padding:30px 60px;">
+    <input class="easyui-searchbox" data-options="prompt:'Ingrese cédula',searcher:doSearch" style="width:100%">
+</div>
+
+<script>
+    function doSearch(value){
+        alert('You input: ' + value);
+    }
+</script>
+
--- views/template.php
+++ views/template.php
@@ -7,6 +7,11 @@
     <link rel="stylesheet" href="css/estilos.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="easyui/themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="easyui/themes/icon.css">
+    <link rel="stylesheet" type="text/css" href="../demo.css">
+    <script type="text/javascript" src="easyui/jquery.min.js"></script>
+    <script type="text/javascript" src="easyui/jquery.easyui.min.js"></script>
 
     <title>Equipo Alfa Buena Maravilla Onda Dinamita Escuadron Lobo </title>
 </head>
